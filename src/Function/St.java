fce25b78cb08587ce251a11b2799dff0cba8373f
ohby00 <quddnr217@naver.com>
2023-08-08 19:43:50 +0900

    8.8일

---
+++ src/Function/St.java
@@ -0,0 +1,17 @@
+package Function;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class St {
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(a).append(b);
+        System.out.println(sb);
+    }
+}
--- src/Step3/B11022.java
+++ src/Step3/B11022.java
@@ -1,4 +1,23 @@
 package Step3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class B11022 {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= T; i++) {
+            String str = br.readLine();
+            int A = str.charAt(0)-'0';
+            int B = str.charAt(2)-'0';
+            sb.append("Case #").append(i).append(": ").append(A)
+                    .append(" + ").append(B).append(" = ").append(A+B).append('\n');
+        }
+        System.out.println(sb);
+    }
 }
