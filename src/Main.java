ad25b21047260adf1cf6201f5e43fec24686425f
ohby00 <quddnr217@naver.com>
2023-09-04 22:20:19 +0900

    9.4일

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+        String arr[][] = new String[5][15];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n-i; j++) {
-                sb.append(" ");
+        for (int i = 0; i < 5; i++) {
+            String str1 = st.nextToken();
+            char A = str1.charAt(i);
+            for (int j = 0; j < 15; j++) {
+                arr[i][j] = String.valueOf(A);
+                sb.append(arr[i][j]);
             }
-            for (int j = 1; j <= 2*i-1; j++) {
-                sb.append("*");
-            }
-            sb.append("\n");
         }
-
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
