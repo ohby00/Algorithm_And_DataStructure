54ff0dda21c967ba48c546129bcf58429ad212e5
ohby00 <quddnr217@naver.com>
2023-09-21 16:47:28 +0900

    9.21일

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,25 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int A = 0;
+        int B = 0;
 
-        while (true) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-
-            if (A == 0 && B == 0) {
-                break;
-            }
-
-            if(A < B){
-                if(B % A == 0){
-                    sb.append("factor").append("\n");
-                }else {
-                    sb.append("neither").append("\n");
-                }
-
-            }else if( A > B) {
-                if(A % B == 0){
-                    sb.append("multiple").append("\n");
-                }else {
-                    sb.append("neither").append("\n");
+            int M = Integer.parseInt(st.nextToken());
+            for (int j = 1; j < M; j++) {
+                A = M / j;
+                if(A == 1){
+                    System.out.println(A);
                 }
             }
+
         }
-        System.out.println(sb);
     }
 }
