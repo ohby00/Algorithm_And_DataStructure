4cd9897ff635765ae9edeac7d754faa56b8d0478
ohby00 <quddnr217@naver.com>
2023-12-26 22:01:58 +0900

    12.26

--- src/Fast_cam/ch_01/F_06_13223.java
+++ src/Fast_cam/ch_01/F_06_13223.java
@@ -1,4 +1,22 @@
 package Fast_cam.ch_01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
 public class F_06_13223 {
+    public static String[] split_array (String str1){
+        String[] split_AB;
+        split_AB = str1.split(":");
+        return split_AB;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        split_array(str1);
+
+    }
 }
