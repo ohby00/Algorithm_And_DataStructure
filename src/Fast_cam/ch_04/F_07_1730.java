2921a21ce50404f96502acb93a5600e26240ab7f
ohby00 <quddnr217@naver.com>
2024-02-04 15:58:29 +0900

    2.4

--- src/Fast_cam/ch_04/F_07_1730.java
+++ src/Fast_cam/ch_04/F_07_1730.java
@@ -1,4 +1,27 @@
 package Fast_cam.ch_04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class F_07_1730 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+        int x = 0;
+        int y = 0;
+
+        String str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'D')
+                y--;
+            else if (str.charAt(i) == 'U') {
+                if(y > 0)
+                    y++;
+            }
+        }
+
+
+    }
 }
