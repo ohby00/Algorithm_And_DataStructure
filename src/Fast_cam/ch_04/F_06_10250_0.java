7b1f4f1e20a900e274d9724bf2e6302928cff5b0
ohby00 <quddnr217@naver.com>
2024-02-14 20:24:06 +0900

    2.14

--- src/Fast_cam/ch_04/F_06_10250_0.java
+++ src/Fast_cam/ch_04/F_06_10250_0.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
         import java.io.InputStreamReader;
         import java.util.StringTokenizer;
 
-public class F_06_10250_1 {
+public class F_06_10250_0 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
