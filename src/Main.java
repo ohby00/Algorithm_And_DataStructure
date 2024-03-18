e19bbdcef22de3f970db4de16782e97e6976f380
ohby00 <quddnr217@naver.com>
2024-03-18 19:34:26 +0900

    3.18

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        solution();
-    }
-    public static void solution() {
-        int[] arr =  {5, 1, 4};
+    public static void main(String[] args){
 
-        int N = Arrays.stream(arr).sum();
-        int[] answer = new int[N];
-        int M = 0;
-
-        for(int i = 0; i < arr.length; i++){
-            for(int j = M; j < arr[i]; j++){
-                answer[M++] = arr[i];
-            }
-        }
-
-        for (int a : answer) {
-            System.out.println(a);
-        }
     }
 }
+
+
 
 
