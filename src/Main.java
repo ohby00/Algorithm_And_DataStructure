aeb9fbe0ad21919afddd58475a84eb5853a98343
ohby00 <quddnr217@naver.com>
2024-03-19 21:05:31 +0900

    3.19

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,34 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        solution();
     }
+    public static void solution() {
+
+          int i = 0;
+        int[] arr = {1, 4, 2, 5, 3};
+        Stack<Integer> stack = new Stack<>();
+
+        while(i < arr.length){
+            for(int a : arr){
+                if(!stack.isEmpty()){
+                    if(stack.peek() < a){
+                        stack.push(a);
+                        i++;
+                    }else if(stack.peek() >= a){
+                        stack.pop();
+                        stack.push(a);
+                    }
+                }else{
+                    stack.push(a);
+                    i++;
+                }
+            }
+        }
+    }
+
+
 }
 
 
