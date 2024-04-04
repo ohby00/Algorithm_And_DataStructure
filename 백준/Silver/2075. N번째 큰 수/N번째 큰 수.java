bbd874590ccf44fef05332c0fb44b657c205bc9f
예날풀빵 <quddnr217@naver.com>
2024-04-04 14:21:48 +0900

    [Silver II] Title: N번째 큰 수, Time: 804 ms, Memory: 216224 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,34 +1,36 @@
-import java.util.*;
-import java.io.*;
- 
-// https://www.acmicpc.net/problem/2075
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
- 
-        int n = Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
- 
+
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            int temp = Integer.parseInt(st.nextToken());
-            pq.offer(temp);
+        // 첫줄을 넣어줌
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            pq.offer(num);
         }
- 
-        for(int i=1; i<n; i++) {
+
+        for(int i = 1; i < N; i++) {
+            // 첫줄 넣어줬으니까 1부터 시작
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++) {
-                int temp = Integer.parseInt(st.nextToken());
- 
-                if(pq.peek() < temp) {  // 추가
+            for(int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+
+                if(pq.peek() < num) {  // 추가
                     pq.poll();
-                    pq.offer(temp);
+                    pq.offer(num);
                 }
             }
         }
- 
+
         System.out.println(pq.poll());
     }
 }
