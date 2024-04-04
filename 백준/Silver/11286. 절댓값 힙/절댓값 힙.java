2dabfa9b68bf9399a1d2a60a1c6cda6e4b3955d6
예날풀빵 <quddnr217@naver.com>
2024-04-04 09:34:42 +0900

    [Silver I] Title: 절댓값 힙, Time: 680 ms, Memory: 28656 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) ->{
+            if (Math.abs(o1) == Math.abs(o2)) {
+                return o1 - o2;
+            } else {
+                return Math.abs(o1) - Math.abs(o2);
+            }
+        });
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                int answer = pq.isEmpty() ? 0 : pq.poll();
+                System.out.println(answer);
+            } else {
+                pq.offer(x);
+            }
+        }
+
+
+    }
+}
