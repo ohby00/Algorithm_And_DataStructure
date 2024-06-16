0696c0511771edee3aa0d6fd1a6f2e823c739817
예날풀빵 <quddnr217@naver.com>
2024-06-16 14:39:19 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(angle == 180){
+            answer = 4;
+        }else if(90 < angle && angle < 180){
+            answer = 3;
+        }else if(angle == 90){
+            answer = 2;
+        }else{
+            answer = 1;
+        }
+            
+        return answer;
+    }
+}
